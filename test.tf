641568112b64fafe04d9462b64fff02004ce1f7d
James Cradock <james.cradock@infinity.co>
2023-07-18 15:05:57 +0100

    test

---
+++ test.tf
@@ -0,0 +1,16 @@
+module "ec2_instance" {
+  source = "github.com/terraform-aws-modules/terraform-aws-ec2-instance?ref=v5.1.0"
+
+  name = "single-instance"
+
+  instance_type          = "t2.micro"
+  key_name               = "user1"
+  monitoring             = true
+  vpc_security_group_ids = ["sg-12345678"]
+  subnet_id              = "subnet-eddcdzz4"
+
+  tags = {
+    Terraform   = "true"
+    Environment = "dev"
+  }
+}
